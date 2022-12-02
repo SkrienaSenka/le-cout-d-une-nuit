a64c7ee8a025e1aff65f6f8e5c27e6768f587652
abbouizd <dylanabbouize@gmail.com>
2022-12-02 04:56:25 +0100

    all fixed

--- public_html/couloir2etage0.js
+++ public_html/couloir2etage0.js
@@ -4,7 +4,7 @@ function detectClick(event){
     console.log(event.clientX + " " + event.clientY)
     console.log(event.clientX/window.innerWidth + " " + event.clientY/window.innerHeight)
     if (event.clientY/window.innerHeight > 0.37 && event.clientY/window.innerHeight < 0.605){
-        if (event.clientX/window.innerWidth > 0.37 && event.clientX/window.innerWidth < 0.605){
+        if (event.clientX/window.innerWidth > 0.387 && event.clientX/window.innerWidth < 0.41){
             window.location = "couloir2etage0porte.html"
         }
     }
