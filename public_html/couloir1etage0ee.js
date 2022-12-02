a88d0b0c6ca20c4a37d8aa68f2023abe1a92366b
abbouizd <dylanabbouize@gmail.com>
2022-12-02 05:24:42 +0100

    end of the easter egg

--- public_html/couloir1etage0ee.js
+++ public_html/couloir1etage0ee.js
@@ -1,18 +1,14 @@
+alert("Cette page est un easter egg !")
+
+
 document.addEventListener("click", detectClick)
-document.querySelector('#closeButton').addEventListener('click', ()=>{
-    document.querySelector('#imgContainer').classList.add('hidden');
-});
 
 function detectClick(event){
     console.log(event.clientX + " " + event.clientY)
     console.log(event.clientX/window.innerWidth + " " + event.clientY/window.innerHeight)
-    if (event.clientX/window.innerWidth > 0.57 && event.clientX/window.innerWidth < 0.66){
-        if (event.clientY/window.innerHeight > 0.35 && event.clientY/window.innerHeight < 0.57)
-            window.location = "couloir1etage0.html"
-    }
-    else if (event.clientX/window.innerWidth > 0.82 && event.clientX/window.innerWidth < 0.87) {
-        if (event.clientY / window.innerHeight > 0.57 && event.clientY / window.innerHeight < 0.63){
-            document.querySelector('#imgContainer').classList.remove('hidden');
+    if (event.clientY/window.innerHeight > 0.187 && event.clientY/window.innerHeight < 0.678){
+        if (event.clientX/window.innerWidth > 0.80 && event.clientX/window.innerWidth < 0.95){
+            window.location = "couloir2etage0porte.html"
         }
     }
 
