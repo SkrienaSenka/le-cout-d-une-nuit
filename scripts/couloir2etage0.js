50f2b5aa9f35b9adfc9ddf1c9bbef81e63bde042
abbouizd <dylanabbouize@gmail.com>
2022-12-02 04:43:07 +0100

    close from ending the first stage (again)

--- scripts/couloir2etage0.js
+++ scripts/couloir2etage0.js
@@ -3,9 +3,9 @@ document.addEventListener("click", detectClick)
 function detectClick(event){
     console.log(event.clientX + " " + event.clientY)
     console.log(event.clientX/window.innerWidth + " " + event.clientY/window.innerHeight)
-    if (event.clientY/window.innerHeight > 0.485 && event.clientY/window.innerHeight < 0.507){
-        if (event.clientX/window.innerWidth > 0.45 && event.clientX/window.innerWidth < 0.51){
-            window.location = "couloir2etage0.html"
+    if (event.clientY/window.innerHeight > 0.37 && event.clientY/window.innerHeight < 0.605){
+        if (event.clientX/window.innerWidth > 0.37 && event.clientX/window.innerWidth < 0.605){
+            window.location = "couloir2etage0porte.html"
         }
     }
 
---
+++ scripts/couloir2etage0porte.js
@@ -0,0 +1,19 @@
+document.addEventListener("click", detectClick)
+document.querySelector('#closeButton').addEventListener('click', ()=>{
+    document.querySelector('#imgContainer').classList.add('hidden');
+});
+
+function detectClick(event){
+    console.log(event.clientX + " " + event.clientY)
+    console.log(event.clientX/window.innerWidth + " " + event.clientY/window.innerHeight)
+    if (event.clientX/window.innerWidth > 0.104 && event.clientX/window.innerWidth < 0.27){
+        if (event.clientY/window.innerHeight > 0.40 && event.clientY/window.innerHeight < 0.73)
+            window.location = "couloir2etage0.html"
+    }
+    else if (event.clientX/window.innerWidth > 0.70 && event.clientX/window.innerWidth < 0.75) {
+        if (event.clientY / window.innerHeight > 0.44 && event.clientY / window.innerHeight < 0.50){
+            document.querySelector('#imgContainer').classList.remove('hidden');
+        }
+    }
+
+}
